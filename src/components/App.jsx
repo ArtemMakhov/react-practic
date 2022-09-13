8a792808f3c3f6d2ba760b54b45172a2c9c69319
Artem Makhov <makhovartem23@gmail.com>
2022-09-13 12:32:06 +0300

    practic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,42 @@
-import { UserMenu } from "path/UserMenu";
+import { Routes, Route } from "react-router-dom";
+import { About } from "../pages/About";
+import { Home } from "../pages/Home";
+import { Products } from "../pages/Products";
+import { ProductDetails } from "../pages/ProductDetails";
+import { NotFound } from "../pages/NotFound";
+import { Mission } from "./Mission";
+import { Reviews } from "./Reviews";
+import { Team } from "./Team";
+import { Container, Header, Logo, Link } from "./App.styled";
 
 export const App = () => {
     return (
-        <div>
-            <UserMenu />
-        </div>
+        <Container>
+            <Header>
+                <Logo>
+                    <span role="img" aria-label="computer icon">
+                    💻 </span>{" "}
+          GoMerch Store
+                </Logo>
+                    <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/products">Products</Link>
+        </nav>
+            </Header>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />}>
+                    <Route path="mission" element={<Mission />} />
+                    <Route path="reviews" element={<Reviews/>} />
+                    <Route path="team" element={<Team/>} />
+                    </Route>
+                    <Route path="/products" element={<Products />} />
+                    <Route path="/products/:id" element={<ProductDetails />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+        </Container>
+
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,19 +4,15 @@ import { ThemeProvider } from 'styled-components';
 import { App } from 'components/App';
 import { theme } from './theme';
 
-import { UserContext } from './path/userContext';
-import {UserProvider} from './path/userContext'
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     
     <ThemeProvider theme={theme}>
-      <UserContext.Provider value={{username: "Mango"}}>
-        <UserProvider>
-          <App />
-        </UserProvider>
-        
-        </UserContext.Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
     
   </React.StrictMode>
